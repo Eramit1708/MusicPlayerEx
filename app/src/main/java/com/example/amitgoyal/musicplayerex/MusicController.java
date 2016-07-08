eab0bfd7b8dd845aa591359b065e566f58947cc8
Eramit1708 <amit.goyal1708@gmail.com>
2016-07-08 16:59:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/amitgoyal/musicplayerex/MusicController.java
@@ -0,0 +1,17 @@
+package com.example.amitgoyal.musicplayerex;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by amit.goyal on 07-07-2016.
+ */
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
